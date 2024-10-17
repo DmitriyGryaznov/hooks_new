a1e665e5cfc502dc69f13fd7e1f95593fc686fda
DmitriyGryaznov <dmitriygryaznov4151@yandex.ru>
2024-10-17 12:02:54 +0300

    prav

--- src/components/CounterOfUnfinishedTasks.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function CounterOfUnfinishedTasks(props) {
-  return <span className="todo-count">{`${props.countOfUnfinishedTasks()} items left`}</span>;
-}
-
-export default CounterOfUnfinishedTasks;
